f88c1b590532efe0d33eba0cde6ccbc218ed2bb8
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-02-22 09:01:11 -0500

    Add service project

---
+++ ProcessHooker.Service/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Serilog;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProcessHooker.Service {
+    class Program {
+
+        static void Main(string[] args) {
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(CreateConfigurationBuilder().Build())
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args)
+                .UseSystemd()
+                .UseSerilog()
+                .UseWindowsService()
+                .ConfigureServices(services => {
+                    services.AddHostedService<Service>();
+                });
+        }
+
+        private static IConfigurationBuilder CreateConfigurationBuilder() {
+             return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+        }
+    }
+}
---
+++ ProcessHooker.Service/Service.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace ProcessHooker.Service {
+    public class Service : BackgroundService {
+
+        private readonly ILogger<Service> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Service(ILogger<Service> logger, IConfiguration configuration) {
+
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+
+            return Task.CompletedTask;
+        }
+    }
+}
