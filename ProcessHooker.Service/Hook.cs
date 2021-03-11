9ea35b6eaf59d7b903a00f062cafa8ffd326beba
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-03-11 10:20:27 -0500

    Add Serializable attribute to hook.cs

--- ProcessHooker.Service/Hook.cs
+++ ProcessHooker.Service/Hook.cs
@@ -1,11 +1,14 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace ProcessHooker.Service {
     /// <summary>
     /// Maps hooks from the appsettings.json file
     /// </summary>
     // TODO - Change the properties names they are every misleading
+    [Serializable]
     public record Hook(string Name, string HookedFilePath) {
+        
         public string HookedFileName {
             get {
                 return this.HookedFilePath
