dae64e64a6b37c9dc0bbedfe93cf3a9949f978e6
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-03-11 11:57:51 -0500

    Remove TODO item

--- ProcessHooker.Service/Hook.cs
+++ ProcessHooker.Service/Hook.cs
@@ -5,7 +5,6 @@ namespace ProcessHooker.Service {
     /// <summary>
     /// Maps hooks from the appsettings.json file
     /// </summary>
-    // TODO - Change the properties names they are every misleading
     [Serializable]
     public record Hook(string HookedProcessName, string FilePath) {
         public string FileName => Path.GetFileName(this.FilePath);
