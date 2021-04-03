288bed9d3cde1233f6a09c95a3ddcc78af7bec4a
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-03 02:58:51 -0400

    Fix HooksSectionParserTests test

--- ProcessHooker.Service.UnitTests/HooksSectionParserTests.cs
+++ ProcessHooker.Service.UnitTests/HooksSectionParserTests.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using Xunit;
 using AutoFixture;
 using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using Serilog;
@@ -10,11 +12,12 @@ namespace ProcessHooker.Service.UnitTests {
     public class HooksSectionParserTests {
         private readonly HooksSectionParser          _sut;
         private readonly Fixture                     _fixture;
-        private readonly ILogger<HooksSectionParser> _logger = Substitute.For<ILogger<HooksSectionParser>>();
+        private readonly IValidator<Hook>            _validator = Substitute.For<IValidator<Hook>>();
+        private readonly ILogger<HooksSectionParser> _logger    = Substitute.For<ILogger<HooksSectionParser>>();
 
         public HooksSectionParserTests() {
             _fixture = new Fixture();
-            _sut     = new HooksSectionParser(new HookValidator(), _logger);
+            _sut     = new HooksSectionParser(_validator, _logger);
         }
 
         [Fact]
@@ -23,6 +26,10 @@ namespace ProcessHooker.Service.UnitTests {
                 _fixture
                     .CreateMany<Hook>(3)
                     .ToArray();
+
+            var hookValidation = new ValidationResult();
+
+            _validator.Validate(Arg.Any<Hook>()).Returns(hookValidation);
 
             var expected = _sut
                 .Parse(Factory.CreateConfigurationSection(actual));
