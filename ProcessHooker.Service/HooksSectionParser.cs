d04f91c4d6e7fd4e8aec778da07b1537a54d487f
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-03 02:26:28 -0400

    Add hook validation to HooksSectionParser

--- ProcessHooker.Service/HooksSectionParser.cs
+++ ProcessHooker.Service/HooksSectionParser.cs
@@ -1,24 +1,51 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
 using System.Text.Json;
+using FluentValidation;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ProcessHooker.Service {
-    // TODO - Add FluentValidation to validate if the hooks are valid
     public class HooksSectionParser : IHooksSectionParser {
+        private readonly IValidator<Hook>            _validator;
+        private readonly ILogger<HooksSectionParser> _logger;
+
+        public HooksSectionParser(IValidator<Hook> validator, ILogger<HooksSectionParser> logger) {
+            _validator = validator;
+            _logger    = logger;
+        }
+
         public IEnumerable<Hook> Parse(IConfigurationSection section) {
-            return section
-                   .AsEnumerable(true)
-                   .GroupBy(pair => pair.Key.Split(":")[0])
-                   .Select(
-                       group => string.Join(
-                           ",",
-                           group.Where(pair => pair.Value is not null)
-                                .Select(pair => $"\"{pair.Key[2..]}\": \"{pair.Value}\"")
-                       )
-                   )
-                   .Select(hooksProperties => JsonSerializer.Deserialize<Hook>($"{{{hooksProperties}}}"))
-                   .ToList();
+            var hooks = section
+                        .AsEnumerable(true)
+                        .GroupBy(pair => pair.Key.Split(":")[0])
+                        .Select(
+                            group => string.Join(
+                                ",",
+                                group.Where(pair => pair.Value is not null)
+                                     .Select(pair => $"\"{pair.Key[2..]}\": \"{pair.Value}\"")
+                            )
+                        )
+                        .Select(hooksProperties => JsonSerializer.Deserialize<Hook>($"{{{hooksProperties}}}"))
+                        .ToList();
+
+            return hooks.Where(
+                hook => {
+                    var validation = _validator.Validate(hook);
+
+                    if(!validation.IsValid) {
+                        _logger.LogInformation(
+                            "Hook is invalid. Error: {Errors}",
+                            string.Join("\n", validation.Errors)
+                        );
+
+                        return false;
+                    }
+
+                    return true;
+                }
+            );
         }
     }
 }
