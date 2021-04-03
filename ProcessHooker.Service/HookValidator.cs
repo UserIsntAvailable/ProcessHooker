091ca9ef82b3d6fb56aa63fbf0e33b5f9c376687
UserIsntAvailable <PinedaP.Angel@gmail.com>
2021-04-03 02:12:21 -0400

    Add HookValidator

---
+++ ProcessHooker.Service/HookValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using FluentValidation;
+
+namespace ProcessHooker.Service {
+    public class HookValidator: AbstractValidator<Hook> {
+        
+        public HookValidator() {
+            this.RuleFor(hook => hook.HookedProcessName)
+                .NotEmpty()
+                .NotNull();
+
+            this.RuleFor(hook => hook.FilePath)
+                .NotEmpty()
+                .NotNull()
+                .Must(File.Exists).WithMessage("{PropertyName} ( {PropertyValue} ) doesn't exists.");
+        }
+    }
+}
